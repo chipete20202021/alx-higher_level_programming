793e16a697d9d7713f4279d573feb4e2c6d4107d
chipete20202021 <chipete20212020@gmail.com>
2023-03-13 05:00:42 -0700

    my first commit

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
+def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        return
 
-my_list = [1, 2, 3]
-print_reversed_list_integer(my_list)
+    my_list.reverse()
+    for list in range(len(my_list)):
+        print("{:d}".format(my_list[list]))
